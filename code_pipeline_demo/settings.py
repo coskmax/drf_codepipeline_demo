82b7b3748133aa29657ff02b031c6fc786852d48
Maxwel Koske <coskmax@gmail.com>
2021-09-24 02:46:42 +0300

    update buildspec

--- code_pipeline_demo/settings.py
+++ code_pipeline_demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-08at&89zoz6%qs4*jzsn#q_1+5f5#9ocfhhws_2w-1f51f$)&n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
